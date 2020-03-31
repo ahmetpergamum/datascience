cd4ea4c5f684f7b85824c26ea34dc8ea9549a786
ahmetpergamum <ahmet.pergamum@gmail.com>
2020-03-31 13:41:57 +0300

    sql join cte

--- 05_sql_essantials/05_query_01.sql
+++ 05_sql_essantials/05_query_01.sql
@@ -1,6 +1,14 @@
+WITH ulke_buyukluk AS(
 SELECT 
 	country as ulke,
-	COUNT(*) musteri_sayisi
+	COUNT(*) musteri_sayisi,
+CASE WHEN COUNT(*) > 20 THEN 'buyuk ulke'
+	WHEN COUNT(*) < 10 THEN 'kucuk ulke'
+	WHEN COUNT(*) >= 10 AND COUNT(*) <= 20 THEN 'orta ulke'
+END AS buyukluk
 FROM customers
 GROUP BY country
-ORDER BY musteri_sayisi;
+ORDER BY musteri_sayisi;)
+
+SELECT *
+FROM ulke_buyukluk;
